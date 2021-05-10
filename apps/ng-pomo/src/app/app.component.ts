e4bf2116d9a2215a7031d4b62f64da63220ca698
tkdn <tkdnation@gmail.com>
2021-05-10 17:49:49 +0900

    [from now] 2021/05/10 17:49:49
    diff --git a/apps/ng-pomo/src/app/app.component.html b/apps/ng-pomo/src/app/app.component.html
    index 4e00378..a128403 100644
    --- a/apps/ng-pomo/src/app/app.component.html
    +++ b/apps/ng-pomo/src/app/app.component.html
    @@ -1 +1,10 @@
    -<p>{{ title }}</p>
    +<h1>NgPomo App</h1>
    +
    +<p>Pomodoro Timer</p>
    +
    +<ng-container *ngIf="remainingMs$ | async as remainingMs">
    +  <p>pomo: {{ remainingMs }}</p>
    +</ng-container>
    +
    +<button (click)="start()">Start</button>
    +<button (click)="stop()">Stop</button>
    diff --git a/apps/ng-pomo/src/app/app.component.ts b/apps/ng-pomo/src/app/app.component.ts
    index b4a2e13..97ee23f 100644
    --- a/apps/ng-pomo/src/app/app.component.ts
    +++ b/apps/ng-pomo/src/app/app.component.ts
    @@ -1,4 +1,12 @@
     import { Component } from '@angular/core';
    +import {
    +  BehaviorSubject,
    +  interval,
    +  NEVER,
    +  Observable,
    +  Subscription,
    +} from 'rxjs';
    +import { scan, switchMap, takeWhile } from 'rxjs/operators';
    
     @Component({
       selector: 'ng-pomo-root',
    @@ -6,5 +14,38 @@ import { Component } from '@angular/core';
       styleUrls: ['./app.component.css'],
     })
     export class AppComponent {
    -  title = 'ng-pomo';
    +  remainingMs$: Observable<number>;
    +  subscription: Subscription;
    +  interval = 1000;
    +  pomoInterval = 5000; // 1000 * 60 * 25;
    +  toggle$ = new BehaviorSubject(true);
    +
    +  start(): void {
    +    this.remainingMs$ = this.toggle$.pipe(
    +      switchMap((resume) => {
    +        return resume ? interval(this.interval) : NEVER;
    +      }),
    +      scan((acc) => {
    +        return acc - this.interval;
    +      }, this.pomoInterval),
    +      takeWhile((time) => {
    +        console.log('RemainingMs:', time);
    +        return time >= 0;
    +      })
    +    );
    +  }
    +
    +  stop(): void {
    +    this.toggle$.next(!this.toggle$.value);
    +    console.log(this.toggle$);
    +  }
    +
    +  toMinutesAndSeconds(ms: number): string {
    +    const minutes = Math.floor(ms / this.interval / 60);
    +    const seconds = String(Math.floor(ms / this.interval) % 60).padStart(
    +      2,
    +      '0'
    +    );
    +    return `${minutes}:${seconds}`;
    +  }
     }

--- apps/ng-pomo/src/app/app.component.ts
+++ apps/ng-pomo/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  BehaviorSubject,
+  interval,
+  NEVER,
+  Observable,
+  Subscription,
+} from 'rxjs';
+import { scan, switchMap, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'ng-pomo-root',
@@ -6,5 +14,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-pomo';
+  remainingMs$: Observable<number>;
+  subscription: Subscription;
+  interval = 1000;
+  pomoInterval = 5000; // 1000 * 60 * 25;
+  toggle$ = new BehaviorSubject(true);
+
+  start(): void {
+    this.remainingMs$ = this.toggle$.pipe(
+      switchMap((resume) => {
+        return resume ? interval(this.interval) : NEVER;
+      }),
+      scan((acc) => {
+        return acc - this.interval;
+      }, this.pomoInterval),
+      takeWhile((time) => {
+        console.log('RemainingMs:', time);
+        return time >= 0;
+      })
+    );
+  }
+
+  stop(): void {
+    this.toggle$.next(!this.toggle$.value);
+    console.log(this.toggle$);
+  }
+
+  toMinutesAndSeconds(ms: number): string {
+    const minutes = Math.floor(ms / this.interval / 60);
+    const seconds = String(Math.floor(ms / this.interval) % 60).padStart(
+      2,
+      '0'
+    );
+    return `${minutes}:${seconds}`;
+  }
 }
