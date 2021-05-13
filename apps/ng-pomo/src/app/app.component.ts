73bcbb7c89c861ced0da0e43d3bdb89a9d7d7109
tkdn <tkdnation@gmail.com>
2021-05-13 12:00:51 +0900

    [from now] 2021/05/13 12:00:51
    diff --git a/apps/ng-pomo/src/app/app.component.html b/apps/ng-pomo/src/app/app.component.html
    index 1a959ef..d8fdc8a 100644
    --- a/apps/ng-pomo/src/app/app.component.html
    +++ b/apps/ng-pomo/src/app/app.component.html
    @@ -6,3 +6,6 @@
    
     <h2>Break</h2>
     <ng-pomo-timer [active]="break" [minutes]="5" (completed)="toFocusTime()"></ng-pomo-timer>
    +
    +<h2>Count</h2>
    +<p>{{ count }}</p>
    diff --git a/apps/ng-pomo/src/app/app.component.ts b/apps/ng-pomo/src/app/app.component.ts
    index be75d6d..608109a 100644
    --- a/apps/ng-pomo/src/app/app.component.ts
    +++ b/apps/ng-pomo/src/app/app.component.ts
    @@ -6,10 +6,12 @@ import { Component } from '@angular/core';
       styleUrls: ['./app.component.css'],
     })
     export class AppComponent {
    +  count = 0;
       focus = true;
       break = false;
    
       toFocusTime(): void {
    +    this.count++;
         this.focus = true;
         this.break = false;
       }

--- apps/ng-pomo/src/app/app.component.ts
+++ apps/ng-pomo/src/app/app.component.ts
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  count = 0;
   focus = true;
   break = false;
 
   toFocusTime(): void {
+    this.count++;
     this.focus = true;
     this.break = false;
   }
